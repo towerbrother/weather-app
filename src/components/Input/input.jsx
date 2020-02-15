3ff963a6311b7ea4c78ff2ef30167e06f479fd99
Giorgio Torre <giorgio.torre8@gmail.com>
2020-02-15 12:21:10 +0000

    first commit 2020

--- src/components/Input/input.jsx
+++ src/components/Input/input.jsx
@@ -3,6 +3,7 @@ import "./Input.css";
 
 const Input = ({ label, ...props }) => (
   <>
+    {/* the logic below ensures to display only if label is truthy */}
     {label && <label htmlFor={props.id}>{label}</label>}
     <input {...props} />
   </>
