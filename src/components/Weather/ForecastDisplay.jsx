fe6453a3b084da715b4587bf2c163cacc9c4c2dd
Giorgio Torre <giorgio.torre8@gmail.com>
2020-03-09 06:06:13 +0000

    new Component ForecastDisplay built, including css

---
+++ src/components/Weather/ForecastDisplay.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import "./ForecastDisplay.css";
+
+const ForecastDisplay = props => {
+  const { dataForecast, unitMeasure } = props;
+
+  const displayWeatherIcon = iconId => {
+    return (
+      <img
+        id="icon"
+        src={`http://openweathermap.org/img/wn/${iconId}@2x.png`}
+        alt="Weather icon"
+      />
+    );
+  };
+
+  const convertCelsius = temp => Math.round((temp - 273.15) * 10) / 10;
+
+  const convertFahrenheit = celsius =>
+    Math.round((celsius * 1.8 + 32) * 10) / 10;
+
+  const arrayForecast = dataForecast.list.filter(
+    item =>
+      item.dt_txt.slice(11, 19) === dataForecast.list[39].dt_txt.slice(11, 19)
+  );
+
+  return unitMeasure === "metric" ? (
+    <div className="forecast">
+      <div className="grid-container">
+        {arrayForecast.map(item => (
+          <div key={item.dt} className="grid-item">
+            <p className="forecast-date">{item.dt_txt.slice(0, 10)}</p>
+            {displayWeatherIcon(item.weather[0].icon)}
+            <p className="forecast-temp">
+              {convertCelsius(item.main.temp)}&deg;C
+            </p>
+            <p className="forecast-description">
+              {item.weather[0].description}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  ) : (
+    <div className="forecast">
+      <div className="grid-container">
+        {arrayForecast.map(item => (
+          <div key={item.dt} className="grid-item">
+            <p className="forecast-date">{item.dt_txt.slice(0, 10)}</p>
+            {displayWeatherIcon(item.weather[0].icon)}
+            <p className="forecast-temp">
+              {convertFahrenheit(convertCelsius(item.main.temp))}&deg;F
+            </p>
+            <p className="forecast-description">
+              {item.weather[0].description}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ForecastDisplay;
--- src/components/Weather/weather.jsx
+++ src/components/Weather/weather.jsx
@@ -1,75 +1,11 @@
 import React from "react";
 import CityNameDisplay from "./CityNameDisplay";
-import "./Weather.css";
 import CurrentDisplay from "./CurrentDisplay";
+import ForecastDisplay from "./ForecastDisplay";
+import "./Weather.css";
 
 const Weather = props => {
   const { dataCurrent, dataForecast, unitMeasure } = props;
-
-  const displayWeatherIcon = iconId => {
-    return (
-      <img
-        id="icon"
-        src={`http://openweathermap.org/img/wn/${iconId}@2x.png`}
-        alt="Weather icon"
-      />
-    );
-  };
-
-  const convertCelsius = temp => Math.round((temp - 273.15) * 10) / 10;
-
-  const convertFahrenheit = celsius =>
-    Math.round((celsius * 1.8 + 32) * 10) / 10;
-
-  const displayForecastMetric = () => {
-    const arrayForecast = dataForecast.list.filter(
-      item =>
-        item.dt_txt.slice(11, 19) === dataForecast.list[39].dt_txt.slice(11, 19)
-    );
-    return (
-      <div className="forecast">
-        <div className="grid-container">
-          {arrayForecast.map(item => (
-            <div key={item.dt} className="grid-item">
-              <p className="forecast-date">{item.dt_txt.slice(0, 10)}</p>
-              {displayWeatherIcon(item.weather[0].icon)}
-              <p className="forecast-temp">
-                {convertCelsius(item.main.temp)}&deg;C
-              </p>
-              <p className="forecast-description">
-                {item.weather[0].description}
-              </p>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  };
-
-  const displayForecastFahrenheit = () => {
-    const arrayForecast = dataForecast.list.filter(
-      item =>
-        item.dt_txt.slice(11, 19) === dataForecast.list[39].dt_txt.slice(11, 19)
-    );
-    return (
-      <div className="forecast">
-        <div className="grid-container">
-          {arrayForecast.map(item => (
-            <div key={item.dt} className="grid-item">
-              <p className="forecast-date">{item.dt_txt.slice(0, 10)}</p>
-              {displayWeatherIcon(item.weather[0].icon)}
-              <p className="forecast-temp">
-                {convertFahrenheit(convertCelsius(item.main.temp))}&deg;F
-              </p>
-              <p className="forecast-description">
-                {item.weather[0].description}
-              </p>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  };
 
   if (unitMeasure === "metric") {
     if (dataCurrent && !dataForecast) {
@@ -89,7 +25,10 @@ const Weather = props => {
             dataCurrent={dataCurrent}
             dataForecast={dataForecast}
           />
-          {displayForecastMetric()}
+          <ForecastDisplay
+            dataForecast={dataForecast}
+            unitMeasure={unitMeasure}
+          />
         </>
       );
     } else {
@@ -100,7 +39,10 @@ const Weather = props => {
             dataForecast={dataForecast}
           />
           <CurrentDisplay dataCurrent={dataCurrent} unitMeasure={unitMeasure} />
-          {displayForecastMetric()}
+          <ForecastDisplay
+            dataForecast={dataForecast}
+            unitMeasure={unitMeasure}
+          />
         </>
       );
     }
@@ -133,7 +75,10 @@ const Weather = props => {
             dataForecast={dataForecast}
           />
           <CurrentDisplay dataCurrent={dataCurrent} unitMeasure={unitMeasure} />
-          {displayForecastFahrenheit()}
+          <ForecastDisplay
+            dataForecast={dataForecast}
+            unitMeasure={unitMeasure}
+          />
         </>
       );
     }
