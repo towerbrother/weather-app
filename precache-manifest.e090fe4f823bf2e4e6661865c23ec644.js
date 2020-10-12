dee5c524f72dd2d4806e0aeefa9ae08e83be97da
Giorgio Torre <giorgio.torre8@gmail.com>
2020-10-12 18:10:52 +0100

    Updates

---
+++ precache-manifest.e090fe4f823bf2e4e6661865c23ec644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "281d886de8e4532be028d90e709ff57e",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "aebe45facf7b79554ffb",
+    "url": "/weather-app/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "4f3d6302810d99923f60",
+    "url": "/weather-app/static/css/main.b41a50e3.chunk.css"
+  },
+  {
+    "revision": "aebe45facf7b79554ffb",
+    "url": "/weather-app/static/js/2.cabe9574.chunk.js"
+  },
+  {
+    "revision": "4f3d6302810d99923f60",
+    "url": "/weather-app/static/js/main.bc861126.chunk.js"
+  },
+  {
+    "revision": "50833e77dc74011a2f7f",
+    "url": "/weather-app/static/js/runtime-main.32031a9c.js"
+  }
+]);
