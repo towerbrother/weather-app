8b9694dca1962059b0fa9362d86842c5e80b0473
Giorgio Torre <giorgio.torre8@gmail.com>
2020-11-08 10:12:42 +0000

    Test radio button for checking and unchecking

--- src/components/Input.test.js
+++ src/components/Input.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
 import Input from "./Input";
 
 describe("Input component when type text", () => {
@@ -18,9 +19,20 @@ describe("Input component when type text", () => {
 
 describe("Input component when type radio", () => {
   const testLabel = "Test label";
-  render(<Input type="radio" label={testLabel} />);
+  const onChange = jest.fn();
+  render(
+    <Input type="radio" label={testLabel} checked={false} onChange={onChange} />
+  );
   const radio = screen.getByLabelText(testLabel);
   it("renders correctly", () => {
     expect(radio).not.toBeNull();
+  });
+  // https://github.com/testing-library/jest-dom/issues/107
+  it("is unchecked correctly", () => {
+    expect(radio).not.toBeChecked();
+  });
+  it("is checked correctly", () => {
+    radio.checked = true;
+    expect(radio).toBeChecked();
   });
 });
