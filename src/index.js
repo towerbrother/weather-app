3bda459a101038d1d65b2ce052da217df37a89e4
GTO <giorgio.torre@stellenanzeigen.de>
2022-02-11 07:18:38 +0100

    added strictmode

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import App from "./components/App";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById("root"));
