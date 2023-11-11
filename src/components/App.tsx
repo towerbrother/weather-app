9a3b2191ef4a1465346acb827c6b97da2257dd8c
towerbrother <giorgio.torre8@gmail.com>
2023-11-11 20:22:34 +0100

    amended URL

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,24 @@
-import React, { useEffect, useState } from "react";
-import Form from "./Form";
-import Weather from "./Weather";
-import Error from "./Error";
-import LoadingPage from "./LoadingPage";
-import Footer from "./Footer";
-import Header from "./Header";
-import WeatherContext from "../context/weatherContext";
-import IError from "../interfaces/IError";
-import { IWeather } from "../interfaces/IWeather";
-import { getWeatherData, getInitialWeatherData } from "../utils/utils";
-import { CURRENT, FORECAST } from "../utils/constants";
+import React, { useEffect, useState } from 'react';
+import Form from './Form';
+import Weather from './Weather';
+import Error from './Error';
+import LoadingPage from './LoadingPage';
+import Footer from './Footer';
+import Header from './Header';
+import WeatherContext from '../context/weatherContext';
+import IError from '../interfaces/IError';
+import { IWeather } from '../interfaces/IWeather';
+import { getWeatherData, getInitialWeatherData } from '../utils/utils';
+import { CURRENT, FORECAST } from '../utils/constants';
 
 const App: () => JSX.Element = () => {
   const [err, setErr] = useState<IError | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [dataCurrent, setDataCurrent] = useState<IWeather | null>(null);
   const [dataForecast, setDataForecast] = useState<IWeather | null>(null);
-  const [unitMeasure, setUnitMeasure] = useState<string>("metric");
-  const [queryStringCity, setQueryStringCity] = useState<string>("");
-  const [queryStringCountry, setQueryStringCountry] = useState<string>("");
+  const [unitMeasure, setUnitMeasure] = useState<string>('metric');
+  const [queryStringCity, setQueryStringCity] = useState<string>('');
+  const [queryStringCountry, setQueryStringCountry] = useState<string>('');
 
   useEffect(() => {
     const currentPositionWeather: () => Promise<void> = async () => {
@@ -50,10 +50,10 @@ const App: () => JSX.Element = () => {
   const handleChange = (e: {
     target: { name: string; value: React.SetStateAction<string> };
   }) => {
-    setQueryStringCountry("");
+    setQueryStringCountry('');
     const name = e.target.name;
-    if (name === "queryStringCity") setQueryStringCity(e.target.value);
-    if (name === "queryStringCountry") setQueryStringCountry(e.target.value);
+    if (name === 'queryStringCity') setQueryStringCity(e.target.value);
+    if (name === 'queryStringCountry') setQueryStringCountry(e.target.value);
   };
 
   const handleSubmit = (e: { preventDefault: () => void }) => {
@@ -79,8 +79,8 @@ const App: () => JSX.Element = () => {
       setLoading
     );
 
-    setQueryStringCity("");
-    setQueryStringCountry("");
+    setQueryStringCity('');
+    setQueryStringCountry('');
   };
 
   const handleRadioChecked = (e: {
@@ -90,10 +90,10 @@ const App: () => JSX.Element = () => {
   };
 
   return (
-    <div className="wrapper">
+    <div className='wrapper'>
       <Header
-        title="Weather App"
-        subtitle="Check the weather before getting out of the house!"
+        title='Weather App'
+        subtitle='Check the weather before getting out of the house!'
       />
       <WeatherContext.Provider
         value={{
@@ -108,8 +108,8 @@ const App: () => JSX.Element = () => {
           handleRadioChecked,
         }}
       >
-        <div className="App">
-          <div className="container">
+        <div className='App'>
+          <div className='container'>
             <Form />
             {err && <Error />}
             {loading ? (
@@ -120,8 +120,8 @@ const App: () => JSX.Element = () => {
           </div>
         </div>
         <Footer
-          author="Giorgio Torre"
-          url="https://towerbrother.github.io/portfolio-app"
+          author='Giorgio Torre'
+          url='https://giorgio-portfolio-towerbrother.vercel.app/'
         />
       </WeatherContext.Provider>
     </div>
